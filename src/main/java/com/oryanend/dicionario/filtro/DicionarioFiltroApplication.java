36ba301ceb8263981f1b770f13be80f5cf586837
oryanend <ryanyamim@gmail.com>
2025-11-12 00:19:01 -0300

    Criação do Projeto

---
+++ src/main/java/com/oryanend/dicionario/filtro/DicionarioFiltroApplication.java
@@ -0,0 +1,13 @@
+package com.oryanend.dicionario.filtro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicionarioFiltroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicionarioFiltroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oryanend/dicionario/filtro/DicionarioFiltroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oryanend.dicionario.filtro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicionarioFiltroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
