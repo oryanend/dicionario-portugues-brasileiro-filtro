04e3df673b0915fce3c925c5eebe338e5cacfce7
oryanend <ryanyamim@gmail.com>
2025-12-16 12:08:02 -0300

    perf: create `WordProjection` interface and add in `WordRepository`

--- src/main/java/com/oryanend/dicionario/filtro/repositories/WordRepository.java
+++ src/main/java/com/oryanend/dicionario/filtro/repositories/WordRepository.java
@@ -1,6 +1,7 @@
 package com.oryanend.dicionario.filtro.repositories;
 
 import com.oryanend.dicionario.filtro.entities.Word;
+import com.oryanend.dicionario.filtro.projections.WordProjection;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,31 +11,33 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WordRepository extends JpaRepository<Word, Long> {
 
-  @Query(value = "SELECT * FROM words ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
-  Word findRandomWord();
-
-  @Query(
-      value = "SELECT * FROM words w WHERE LENGTH(w.word) <= :maxChar ORDER BY RANDOM() LIMIT 1",
-      nativeQuery = true)
-  Optional<Word> findRandomWordByMaxCharSize(@Param("maxChar") int maxChar);
-
-  @Query(
-      value = "SELECT * FROM words w WHERE LENGTH(w.word) >= :minChar ORDER BY RANDOM() LIMIT 1",
-      nativeQuery = true)
-  Optional<Word> findRandomWordByMinCharSize(@Param("minChar") int minChar);
-
-  @Query(
-      value = "SELECT * FROM words w WHERE LENGTH(w.word) = :charSize ORDER BY RANDOM() LIMIT 1",
-      nativeQuery = true)
-  Optional<Word> findRandomWordByEqualCharSize(@Param("charSize") int charSize);
+  @Query(value = "SELECT id, word FROM words ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+  WordProjection findRandomWord();
 
   @Query(
       value =
-          "SELECT * FROM words w "
-              + "WHERE LENGTH(w.word) >= :minChar "
-              + "AND LENGTH(w.word) <= :maxChar "
-              + "ORDER BY RANDOM() "
-              + "LIMIT 1",
+          "SELECT id, word FROM words w WHERE LENGTH(w.word) <= :maxChar ORDER BY RANDOM() LIMIT 1",
       nativeQuery = true)
-  Optional<Word> findRandomWordByMinAndMaxCharSize(int minChar, int maxChar);
+  Optional<WordProjection> findRandomWordByMaxCharSize(@Param("maxChar") int maxChar);
+
+  @Query(
+      value =
+          "SELECT id, word FROM words w WHERE LENGTH(w.word) >= :minChar ORDER BY RANDOM() LIMIT 1",
+      nativeQuery = true)
+  Optional<WordProjection> findRandomWordByMinCharSize(@Param("minChar") int minChar);
+
+  @Query(
+      value =
+          "SELECT id, word FROM words w WHERE LENGTH(w.word) = :charSize ORDER BY RANDOM() LIMIT 1",
+      nativeQuery = true)
+  Optional<WordProjection> findRandomWordByEqualCharSize(@Param("charSize") int charSize);
+
+  @Query(
+      value =
+          "SELECT id, word FROM words w "
+              + "WHERE LENGTH(w.word) >= :minChar AND LENGTH(w.word) <= :maxChar "
+              + "ORDER BY RANDOM() LIMIT 1",
+      nativeQuery = true)
+  Optional<WordProjection> findRandomWordByMinAndMaxCharSize(
+      @Param("minChar") int minChar, @Param("maxChar") int maxChar);
 }
